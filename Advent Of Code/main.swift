8c9e0a3c64b82ae4bc3929b833c303e74f71570b
Valentin Cernuta <vcernuta3@gmail.com>
2020-12-07 13:39:46 +0100

    Initial Commit

---
+++ Advent Of Code/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Advent Of Code
+//
+//  Created by Valentin Cernuta on 05/12/2020.
+//
+
+import Foundation
+
+//print("\nDay 1 \n ========== ")
+//day1()
+
+//print("\nDay 2 \n ========== ")
+//day2()
+
+//print("\nDay 3 \n ========== ")
+//day3()
+
+//print("\nDay 4 \n ========== ")
+//day4()
+
+//print("\nDay 5 \n ========== ")
+//day5()
+
+print("\nDay 6 \n ========== ")
+day6()
